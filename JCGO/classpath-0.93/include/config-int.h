4ae6b302da85bc3ae5cd780863e080a4ed574d4c
Mister Hat <misterhat144@gmail.com>
2021-07-16 22:11:37 -0500

    add rest of JCGO

---
+++ JCGO/classpath-0.93/include/config-int.h
@@ -0,0 +1,9 @@
+#ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
+#define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "classpath 0.93"
+/* generated using gnu compiler gcc (GCC) 4.1.1 20060525 (Red Hat 4.1.1-1) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
