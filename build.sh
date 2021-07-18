ea97265d0af6e68f9920df16df88fe6fa3bee508
Mister Hat <misterhat144@gmail.com>
2021-07-18 12:16:31 -0500

    windows garbagecollector, fix gcc -O2

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ patch -p0 -i sdl.diff
 
     #-DJCGO_PARALLEL
     #-DJCGO_NOGJ
-gcc -g -I ./JCGO/include -I ./JCGO/native -I ./JCGO/include/boehmgc \
+gcc -O2 -I ./JCGO/include -I ./JCGO/native -I ./JCGO/include/boehmgc \
+    -fno-strict-aliasing \
     -DJCGO_USEGCJ -DJCGO_FPFAST -DJCGO_FASTMATH -DJCGO_INET  \
     -DJCGO_INTFIT -DJCGO_THREADS -DJCGO_UNIX -D_IEEEFP_H -DJCGO_UNIFSYS \
     -o mudclient jcgo_Out/Main.c -lm -lpthread -lSDL2 \
